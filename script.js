98e18febd22e8c3592a96b9acc5c44335e0b34d9
AndyMagwayer <andymagwayer@gmial.com>
2023-08-08 10:51:26 +0500

    feat: added object in centre like universe and also button which change and do fullscreen(10:50)

--- script.js
+++ script.js
@@ -197,20 +197,19 @@ function onWindowResize() {
 
 
 
-  Fullscreen btn 
+//    Fullscreen btn    
  let fullscreen;
  let fsEnter = document.getElementById('fullscr');
 fsEnter.addEventListener('click', function (e) {
     e.preventDefault();
-//      if (!fullscreen) {
-          fullscreen = true;
-          document.documentElement.requestFullscreen();
-          fsEnter.innerHTML = "Exit Fullscreen";
-      }
-      else {
-          fullscreen = false;
-          document.exitFullscreen();
-          fsEnter.innerHTML = "Go Fullscreen";
-      }
-  });
-
+    if (!fullscreen) {
+        fullscreen = true;
+        document.documentElement.requestFullscreen();
+        fsEnter.innerHTML = "Exit Fullscreen";
+    }
+   else {
+        fullscreen = false;
+       document.exitFullscreen();
+       fsEnter.innerHTML = "Go Fullscreen";
+   }
+ });
