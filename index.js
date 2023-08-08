4272d40f1615c8b6dea2626f0a84a971798851c6
AndyMagwayer <andymagwayer@gmial.com>
2023-08-08 10:41:33 +0500

    feat: added getElementByIdsection and detalizition codes browse (10:40 )

---
+++ index.js
@@ -0,0 +1,5 @@
+let renderer,scene,camera,sphereBg,nucleus,stars,controls,container = document.getElementByld("canvas_container"),
+timeout_Debounce,noise = new SimplexNoise(),
+cameraSpeed = 0,
+blobScale =3;
+
